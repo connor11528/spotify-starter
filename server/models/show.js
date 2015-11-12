ec2d206f0ea12e433220be7ae3cb081a7cfd4fc7
Connor Leech <connorleech@gmail.com>
2015-11-12 13:20:44 -0800

    format show list

--- server/models/show.js
+++ server/models/show.js
@@ -3,7 +3,9 @@ var Schema = mongoose.Schema;
 
 var showSchema = new Schema({
 	title: { type: String, required: true },
-	date: { type: Date, default: Date.now },
+	description: { type: String, default: 'Awesome show description!' },
+	img_thumb: { type: String, default: 'http://placehold.jp/150x150.png' },
+	datetime: { type: Date, default: Date.now },
 	artists: [String] // spotify ids
 });
 
