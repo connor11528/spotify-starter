2f0b02e56b538d0df17437ee7aaf343879ae9de1
Connor Leech <connorleech@gmail.com>
2015-11-17 20:26:14 -0800

    add button layout and login arrow

--- public/client/js/controllers/artist.js
+++ public/client/js/controllers/artist.js
@@ -7,6 +7,7 @@ app.controller('ArtistCtrl', [
 	function($scope, $stateParams, Spotify, Songkick){
 		var artist_spotify_id = $stateParams.id;
 		$scope.artist = {};
+		$scope.loading = true;
 
 		// get artist info from spotify
 		Spotify.getArtist(artist_spotify_id).then(function(res){
@@ -17,6 +18,7 @@ app.controller('ArtistCtrl', [
 
 				Songkick.artistUpcomingEvents(res.id).then(function(upcomingEvents){
 					$scope.upcomingEvents = upcomingEvents;
+					$scope.loading = false;
 				});
 			});
 		});
