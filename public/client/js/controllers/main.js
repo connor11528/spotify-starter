276f3f1d249eec771c7b5ef86714db88cacb64ab
Connor Leech <connorleech@gmail.com>
2015-12-01 20:34:53 -0800

    add popular artists

--- public/client/js/controllers/main.js
+++ public/client/js/controllers/main.js
@@ -2,9 +2,10 @@
 app.controller('MainCtrl', [
 	"$scope", 
 	"$rootScope", 
+	"$q",
 	"Spotify", 
-	"artists", 
-	function($scope, $rootScope, Spotify, artists){
+	"artists",
+	function($scope, $rootScope, $q, Spotify, artists){
 		$scope.currentPage = 0;
 		$scope.searchResults = [];
 
@@ -26,10 +27,28 @@ app.controller('MainCtrl', [
 			$rootScope.artists = artistsUserFollows;
 		});
 
+		// search artists
 		$scope.search = function(searchTerm){
 			Spotify.search(searchTerm, 'artist', {})
 				.then(function(res){
 					$scope.searchResults = res.artists.items;
 				});
 		};
+
+		// get popular artists
+		var popularIds = [
+			'04gDigrS5kc9YWfZHwBETP', // Maroon 5
+			'4pb4rqWSoGUgxm63xmJ8xc', // Madeon
+			'46CitWgnWrvF9t70C2p1Me', // Daughter
+			'21mKp7DqtSNHhCAU2ugvUw', // Odesza
+			'1rCIEwPp5OnXW0ornlSsRl' // Metric
+		];
+		$scope.popularArtists = [];
+		popularIds.forEach(function(id){
+
+			Spotify.getArtist(id).then(function(res){
+				$scope.popularArtists.push(res);
+			});
+		});
+		
 	}]);
